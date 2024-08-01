a6f387d9f3b8f6e5648c874a8e6c3ed477f66d88
Darae0415 <ekfogksmf@naver.com>
2024-08-01 10:26:06 +0900

    5장 컴포넌트 기본복습2

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,43 +1,45 @@
 <template>
-<div class="">
-  <input type="text" value="333"/><br/>
-  <input type="text" v-bind:value="nick"><br/>
-  <input type="text" v-model="nick"><br/>
-  <input type="number" v-model.number="age"><br/>
-  <textarea cols="30" rows="10">동해물과 백두산이</textarea>
-  <textarea v-model="message" cols="30" rows="10"></textarea><br/>
-  <select >
-    <option value="1">서울</option>
-    <option value="2">부산</option>
-    <option value="3" selected>대구</option>
-    <option value="4">수원</option>
-  </select>
-  <br/>
-  <br/>
-  <select v-model="city">
-    <option value="1">서울</option>
-    <option value="2">부산</option>
-    <option value="3">대구</option>
-    <option value="4">수원</option>
-  </select>
-  <br/>
-</div>
+  <div class="">
+    <input type="checkbox" id="cbtn_agree" name="cbtn_agree">
+    <label for="cbtn_agree">동의</label><br/>
+    
+    <label><input type="checkbox" v-model="agree">동의</label><br/>
+    <label><input type="checkbox" v-model="agree2"
+    true-value="동의" false-value="비동의">{{agree2}}</label><br/>
+  </div>
+  <div>
+    <p>기본피자에 원하시는 토핑을 추가하세요.</p>
+    <label><input type="checkbox" v-model="pizza" value="치즈">치즈</label><br/>
+    <label><input type="checkbox" v-model="pizza" value="파인애플">파인애플</label><br/>
+    <label><input type="checkbox" v-model="pizza" value="버섯">버섯</label><br/>
+    <p>당신이 추가로 선택하신 토핑은 {{pizza}} 입니다.</p>
+  </div>
+  <div>
+    <p>당신의 탕수육을 먹는 취향은?</p>
+    <label><input type="radio" v-model="sweet_sour_pork" value="부먹">부먹</label>
+    <label><input type="radio" v-model="sweet_sour_pork" value="찍먹">찍먹</label>
+    <p>당신은 <span style="color:red; font-we">{{sweet_sour_pork}}파</span> 입니다.</p>
+  </div>
 </template>
 
 <script>
-export default{
-  name:'TestView',
-  components:{},
-  data(){
-    return{
-      nick:'노래하는 큰바위',
-      age:30,
-      message:'깊은 산 속 옹달샘 누가와서 먹나요',
-      city:'4',
-    }
+export default {
+  name: 'TestView',
+  components: {
+    
   },
-  methods:{}
-}
+  data() {
+    return {
+      agree:false,
+      agree2:false,
+      pizza:[],
+      sweet_sour_pork:'',
+    };
+  },
+  methods: {
+    
+  }
+};
 </script>
 
 <style scoped>
