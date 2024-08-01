98bbc2ed8269c09ab4a618870dd51369b7c37be9
Darae0415 <ekfogksmf@naver.com>
2024-08-01 10:40:19 +0900

    5장 컴포넌트 기본복습3+빌드

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,25 +1,13 @@
 <template>
-  <div class="">
-    <input type="checkbox" id="cbtn_agree" name="cbtn_agree">
-    <label for="cbtn_agree">동의</label><br/>
-    
-    <label><input type="checkbox" v-model="agree">동의</label><br/>
-    <label><input type="checkbox" v-model="agree2"
-    true-value="동의" false-value="비동의">{{agree2}}</label><br/>
-  </div>
-  <div>
-    <p>기본피자에 원하시는 토핑을 추가하세요.</p>
-    <label><input type="checkbox" v-model="pizza" value="치즈">치즈</label><br/>
-    <label><input type="checkbox" v-model="pizza" value="파인애플">파인애플</label><br/>
-    <label><input type="checkbox" v-model="pizza" value="버섯">버섯</label><br/>
-    <p>당신이 추가로 선택하신 토핑은 {{pizza}} 입니다.</p>
-  </div>
-  <div>
-    <p>당신의 탕수육을 먹는 취향은?</p>
-    <label><input type="radio" v-model="sweet_sour_pork" value="부먹">부먹</label>
-    <label><input type="radio" v-model="sweet_sour_pork" value="찍먹">찍먹</label>
-    <p>당신은 <span style="color:red; font-we">{{sweet_sour_pork}}파</span> 입니다.</p>
-  </div>
+<div>
+  <!-- 속성 연결 -->
+  <img src='https://borgssam.github.io/MySite/img/album_01.jpg' alt="" title="가방 든 사나이"><br/>
+  <img v-bind:src='imgSrc' v-bind:title="imgTitle" alt=""><br/>
+</div>
+<div>
+  <button :disabled="btn_1" :style="color_red">버튼</button>
+  <button :disabled="btn_2" :style="color_blue">버튼</button>
+</div>
 </template>
 
 <script>
@@ -30,10 +18,12 @@ export default {
   },
   data() {
     return {
-      agree:false,
-      agree2:false,
-      pizza:[],
-      sweet_sour_pork:'',
+      imgSrc:'https://borgssam.github.io/MySite/img/album_01.jpg',
+      imgTitle:'가방 든 사나이',
+      btn_1:true,
+      btn_2:false,
+      color_blue:{color:'blue', fontSize:'24px'},
+      color_red:{color:'red', fontSize:'24px'},
     };
   },
   methods: {
