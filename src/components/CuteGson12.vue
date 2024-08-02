95ee9bd1e79b1de428b50528ae0ccc5829f84c4c
Darae0415 <ekfogksmf@naver.com>
2024-08-02 15:35:39 +0900

    8장 컴포넌트(심화7) 자식=>아버지 이벤트전달

--- src/components/CuteGson12.vue
+++ src/components/CuteGson12.vue
@@ -2,6 +2,8 @@
   <div class="gson">
     <!-- 내용을 추가하세요 -->
     <p v-bind:style="{color:tcolor}" >큰집손자 2 : {{message}}</p>
+    <input type="text" v-model="gsonMsg">
+    <input type="button" value="막내손자1" @click="clickGson22">
   </div>
 </template>
 
@@ -9,26 +11,6 @@
 export default {
   name: 'CuteGson12',
   props: {
-    // 문자열 타입의 prop 예시
-    //sampleString: {
-    //  type: String,
-    //  default: ''
-    //},
-    // 숫자 타입의 prop 예시
-    //sampleNumber: {
-    //  type: Number,
-    //  default: 0
-    //},
-    // 배열 타입의 prop 예시
-    //sampleArray: {
-    //  type: Array,
-    //  default: () => []
-    //},
-    // 객체 타입의 prop 예시
-    //sampleObject: {
-    //  type: Object,
-    //  default: () => ({})
-    //}
   },
   components: {
     // 추가적으로 사용할 컴포넌트들을 등록합니다.
@@ -38,6 +20,7 @@ export default {
       message:'',
       index:2,
       tcolor:'red',
+      gsonMsg:'',
     };
   },
   watch: {
@@ -63,7 +46,10 @@ export default {
         this.tcolor=data;
       }
       alert('cuteson12=>'+idx+','+data);
-    }
+    },
+    clickGson22(){
+      this.$emit('event_report',this.gsonMsg);
+    },
   },
   setup() {
     // Vue 3 Composition API의 setup 함수에서 추가적인 로직을 처리할 수 있습니다.
